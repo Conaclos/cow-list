990d908d0a02f60d25dcc196687c7c854eb3a42c
Victorien Elvinger <victorien@elvinger.fr>
2022-06-08 23:40:44 +0200

    refactor: avoid some deopt of lengthOf
    lengthOf becomes a polymrihic function when distinct objects
    are passed through.
    
    To reduce the number of deopt we add typeof guards.

--- src/core/lengthy.ts
+++ src/core/lengthy.ts
@@ -1,12 +1,22 @@
 import type { u32 } from "../util/number.js"
 
 export interface Lengthy {
-    readonly length?: u32 | undefined
+    readonly length?: unknown
 }
 
 /**
  * @param x
  * @return length of {@code x} or 1 if none.
  */
-export const lengthOf = (x: Lengthy): u32 =>
-    x.length !== undefined ? x.length : 1
+export function lengthOf(x: Lengthy): u32 {
+    if (typeof x === "string") {
+        return x.length
+    } else if (typeof x === "object" && x !== null) {
+        if (Array.isArray(x)) {
+            return x.length
+        } else if (typeof x.length === "number") {
+            return x.length
+        }
+    }
+    return 1
+}

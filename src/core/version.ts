236e875b112ab8cc09d331cef49195b33298f60f
Victorien Elvinger <victorien@elvinger.fr>
2022-06-09 19:15:13 +0200

    refactor: de not export utils' types

--- src/core/version.ts
+++ src/core/version.ts
@@ -1,6 +1,4 @@
 //! Copyright (c) 2019 Victorien Elvinger
 //! Licensed under Apache License 2.0 (https://apache.org/licenses/LICENSE-2.0)
 
-import type { u32 } from "../util/number.js"
-
-export type Version = u32
+export type Version = number
